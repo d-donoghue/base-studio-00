c4b61455c4da230b2d1a88b02d49133ddcb92e3e
elieweitzman <elieweitzman@eliesstcomputer.lan>
2022-01-20 13:30:44 -0500

    message

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -4,5 +4,7 @@ public class Message {
 
 	public static void main(String args[]) {
 		System.out.println("Say hi to your group mates!");
+		System.out.println("What is up");
+		System.out.println("Not much how about yourself");
 	}
 }
